c5a8ba1c16a55f515549424daadd94d31d6cb0cd
Jhordy Gavinchu <dev@jhordyess.com>
2022-11-28 20:16:00 -0400

    Added Fetch

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,82 @@
 import React from "react";
 import Papa from "papaparse";
-
+import MUIDataTable from "mui-datatables";
+const columns = [
+  {
+    name: "name",
+    label: "name",
+    options: {
+      filter: false,
+    },
+  },
+  "age",
+  {
+    name: "number",
+    label: "number",
+    options: {
+      filter: false,
+    },
+  },
+  "nationality",
+  "club",
+  {
+    name: "date born",
+    label: "date born",
+    options: {
+      filter: false,
+      display: false,
+    },
+  },
+  "height",
+  "weight",
+  "pos",
+  {
+    name: "feet",
+    label: "feet",
+    options: {
+      display: false,
+    },
+  },
+  "nat team",
+];
 export default function () {
-  const fnt1 = async ({ target }) => {
+  const [data, setData] = React.useState([]);
+
+  const fetchData = async () => {
     try {
       const request = await fetch(
         "https://raw.githubusercontent.com/gersontorrezm/bolivian-soccer/main/soccer-players.csv"
       );
-      const binary = await request.text();
-      const cvs = Papa.parse(binary);
-      const { data } = cvs;
-      alert(`${data.length} registros obtenidos`);
+      const text = await request.text();
+      const _data = Papa.parse(text).data;
+      _data.shift();
+      // _data.pop();
+      setData(_data);
     } catch (error) {
       console.error(error);
     }
   };
 
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  const options = {
+    filterType: "checkbox",
+    responsive: "standard",
+    selectableRows: "none",
+  };
   return (
     <div className="container">
-      <h2>Soccer Info</h2>
-      <div></div>
-      <button onClick={fnt1}>Click me</button>
+      <h2>Futbolistas bolivia</h2>
+      <div>
+        <MUIDataTable
+          title="Lista de jugadores"
+          data={data}
+          columns={columns}
+          options={options}
+        />
+      </div>
       <footer>
         Made with 💪 by{" "}
         <a href="https://jhordyess.com" target="_blank" rel="noreferrer">
