784124e87d44deb51984dd1aaa58f2a82f09d642
Jhordy Gavinchu <dev@jhordyess.com>
2022-11-28 20:17:34 -0400

    Update gitignore and history.sh

--- history.sh
+++ history.sh
@@ -9,3 +9,4 @@ npm i -D css-minimizer-webpack-plugin terser-webpack-plugin clean-webpack-plugin
 
 npm i @mui/material @emotion/react @emotion/styled @mui/icons-material
 npm i mui-datatables papaparse
+npm i -D gh-pages
